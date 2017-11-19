8adaf233811a065c72512887ed66d6bd0c81558d
bmchorse <bmchorse@gmail.com>
2017-11-19 12:24:55 -0500

    Tests for internal calculations of ReversibleRxn()

--- test/test_chemkin.py
+++ test/test_chemkin.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('..')  # Needs to be before chemkin import
 import chemkin
 import cmath
+import numpy as np
 
 client_x = [2, 1, 0.5, 1, 1]
 client_temps = [750, 1500, 2500]
@@ -58,44 +59,48 @@ def test_creation():
     assert(rever.R == 8.3144598)
     assert((rever.gamma == [0, 0, 0]).all())
 
-def test_read_db():
-    rever = chemkin.ReversibleRxn('test/rxnrate.xml')
-    rever.read_SQL(100)
-    assert((rever.nasa[0] == [[2.50000001e+00, -2.30842973e-11,
-                               1.61561948e-14, -4.73515235e-18,
-                               4.98197357e-22,  2.54736599e+04,
-                               -4.46682914e-01]]).all())
+rever = chemkin.ReversibleRxn('test/rxnrate.xml')
 
-def test_trange_greater():
-    pass
+def test_read_db():
+    rever.read_SQL(100)
+    assert((rever.nasa[0] == [2.50000001e+00, -2.30842973e-11,
+                              1.61561948e-14, -4.73515235e-18,
+                              4.98197357e-22,  2.54736599e+04,
+                              -4.46682914e-01]).all())
 
 def test_trange_lesser():
-    pass
-
-def test_getlowcoeffs():
-    pass
-
-def test_gethighcoeffs():
-    pass
+    rever.read_SQL(2500)
+    assert((rever.nasa[0] == [2.50000000e+00, 7.05332819e-13,
+                              -1.99591964e-15, 2.30081632e-18,
+                              -9.27732332e-22, 2.54736599e+04,
+                              -4.46682853e-01]).all())
 
 def test_badT():
-    rever = chemkin.ReversibleRxn('test/rxnrate.xml')
     try:
         rever.read_SQL('I am not a number!')
     except AssertionError as err:
         assert(type(err) == AssertionError)
 
 def test_Cp():
-    pass
+    out = rever.Cp_over_R(100)
+    # Note the use of np.isclose() to handle VERY tiny rounding diffs.
+    assert(np.isclose(out, np.array([2.5, 2.90084906,
+                                     3.79431834, 2.96702128,
+                                     3.57189904])).all())
 
 def test_H():
-    pass
+    out = rever.H_over_RT(2500)
+    assert(np.isclose(out, np.array([12.68946396, 17.12513405, 7.5535748,
+                    -7.51349287, 7.6502427])).all())
 
 def test_S():
-    pass
+    out = rever.S_over_R(2000)
+    assert(np.isclose(out, np.array([18.5555733, 24.97024176, 29.77900614,
+                                     19.75486987,  33.26701985])).all())
 
 def test_backwards_coeffs():
-    pass
+    rever.backward_coeffs(900)
+    assert((rever.kb == [10, 10, 10]).all())
 
 def test_prog_rate_reversible():
     pass
